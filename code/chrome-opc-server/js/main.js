78f4ece786053d8e61af76a88db8c35f97e436d2
Eric Miller <github@patternleaf.com>
2014-11-17 18:18:19 -0700

    Fix opc addressing for physical rig. (#4)
    Note this breaks the ordering within the simulator itself.

--- code/chrome-opc-server/js/main.js
+++ code/chrome-opc-server/js/main.js
@@ -535,12 +535,12 @@ function createLightsForRail(rail, railIndex) {
 		_(['down', 'up']).each(function(stripName) {
 			var stripCurvePts = [];
 			for (var i = 0; i < pair.points[stripName].length; i++) {
-				var physicalAddress = gOPCPointList.length;
-				var opcAddress = physicalAddress;
+				var opcAddress = gOPCPointList.length;
+				var physicalAddress = opcAddress;
 				var stripN = Math.floor(physicalAddress / 60);
 				if (stripN % 2 == 0) {
 					// if floor(i / 60) is even, the strip address is physically reversed
-					opcAddress = (stripN * 60) + (60 - (i % 60)) - 1;
+					physicalAddress = (stripN * 60) + (60 - (i % 60)) - 1;
 				}
 				
 				var sceneLight;
@@ -573,6 +573,14 @@ function createLightsForRail(rail, railIndex) {
 				});
 
 				scene.add(sceneLight);
+				if (0 && railIndex == 0 && ((i % 4) == 0)) {
+					sceneLightLabel = makeTextSprite(physicalAddress + ', ' + opcAddress, {
+						fontSize: 10,
+						borderThickness: 1
+					});
+					sceneLightLabel.position = sceneLight.position.clone();
+					scene.add(sceneLightLabel);
+				}
 				// if (sceneLightLabel) {
 				// 	scene.add(sceneLightLabel);
 				// }
@@ -607,6 +615,78 @@ function createLightsForRail(rail, railIndex) {
 			*/
 		});
 	});
+}
+
+// see http://stemkoski.github.io/Three.js/Sprite-Text-Labels.html
+function makeTextSprite( message, parameters ) {
+	if ( parameters === undefined ) parameters = {};
+	
+	var fontface = parameters.hasOwnProperty("fontface") ? 
+		parameters["fontface"] : "Arial";
+	
+	var fontsize = parameters.hasOwnProperty("fontsize") ? 
+		parameters["fontsize"] : 18;
+	
+	var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+		parameters["borderThickness"] : 4;
+	
+	var borderColor = parameters.hasOwnProperty("borderColor") ?
+		parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+	
+	var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+		parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+
+	//var spriteAlignment = THREE.SpriteAlignment.topLeft;
+		
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = "Bold " + fontsize + "px " + fontface;
+    
+	// get size data (height depends only on font size)
+	var metrics = context.measureText( message );
+	var textWidth = metrics.width;
+	
+	// background color
+	context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+								  + backgroundColor.b + "," + backgroundColor.a + ")";
+	// border color
+	context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+								  + borderColor.b + "," + borderColor.a + ")";
+
+	context.lineWidth = borderThickness;
+	roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+	// 1.4 is extra height factor for text below baseline: g,j,p,q.
+	
+	// text color
+	context.fillStyle = "rgba(0, 0, 0, 1.0)";
+
+	context.fillText( message, borderThickness, fontsize + borderThickness);
+	
+	// canvas contents will be used for a texture
+	var texture = new THREE.Texture(canvas) 
+	texture.needsUpdate = true;
+
+	var spriteMaterial = new THREE.SpriteMaterial({ map: texture, useScreenCoordinates: false });
+	var sprite = new THREE.Sprite(spriteMaterial);
+	//sprite.scale.set(100,50,1.0);
+	sprite.scale.set(25,12.5,0.25);
+	return sprite;	
+}
+
+function roundRect(ctx, x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+	ctx.stroke();   
 }
 
 $('#show-opc-layout').on('click', function() {
